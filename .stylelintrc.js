3ab28d6f58d7184151b9ec5eea4c93fca5d89839
Kola Kolade <kolakolade@gmail.com>
2023-09-28 15:40:43 +0100

    Update content of .scss files

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,42 +1,7 @@
-// {
-//   "extends": ["stylelint-config-recommended-scss"],
-//   "plugins": ["stylelint-scss", "stylelint-csstree-validator"],
-//   "rules": {
-//     "at-rule-no-unknown": [
-//       true,
-//       {
-//         "ignoreAtRules": [
-//           "tailwind",
-//           "apply",
-//           "variants",
-//           "responsive",
-//           "screen",
-//           "@use"
-//         ]
-//       }
-//     ],
-//     "scss/at-rule-no-unknown": [
-//       true,
-//       {
-//         "ignoreAtRules": [
-//           "tailwind",
-//           "apply",
-//           "variants",
-//           "responsive",
-//           "screen",
-//           "@use"
-//         ]
-//       }
-//     ],
-//     "csstree/validator": true
-//   },
-//   "ignoreFiles": ["build/**", "dist/**", "**/reset*.css", "**/bootstrap*.css"]
-// }
 module.exports = {
+  extends: 'stylelint-config-standard',
   plugins: ['stylelint-scss', 'stylelint-csstree-validator'],
   rules: {
-    // Add your SCSS-specific rules here
-
     'at-rule-no-unknown': [
       true,
       {
@@ -63,9 +28,7 @@ module.exports = {
         ],
       },
     ],
-    'csstree/validator': true,
-
-    'at-rule-no-unknown': null, // Disable the at-rule-no-unknown rule
+    'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': true,
     'csstree/validator': true,
   },
