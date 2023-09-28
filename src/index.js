e5b6bc25c5ff547453430797bb81cd74868b88a4
Kola Kolade <kolakolade@gmail.com>
2023-09-28 23:40:56 +0100

    Import fontawesome and add social icons

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import './assets/css/style.css';
 import heroLogo from './assets/images/kolakolade_logo.png';
 import './assets/scss/main.scss';
 
+// Import fontawesome
+import '@fortawesome/fontawesome-free/js/brands';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/solid';
+
+// Site logo
 const logoDiv = document.querySelector('.hero__logo');
 logoDiv.src = heroLogo;
