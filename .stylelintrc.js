da47f2175e7abf9fbf0cc033c1f132742c5537f2
Kola Kolade <kolakolade@gmail.com>
2023-09-28 15:47:38 +0100

    Update content of .scss files

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,32 +2,16 @@ module.exports = {
   extends: 'stylelint-config-standard',
   plugins: ['stylelint-scss', 'stylelint-csstree-validator'],
   rules: {
-    'at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: [
-          'tailwind',
-          'apply',
-          'variants',
-          'responsive',
-          'screen',
-          '@use',
-        ],
-      },
-    ],
-    'scss/at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: [
-          'tailwind',
-          'apply',
-          'variants',
-          'responsive',
-          'screen',
-          '@use',
-        ],
-      },
-    ],
+    'scss/at-extend-no-missing-placeholder': true,
+    'scss/at-function-pattern': '^[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
+    'scss/at-import-no-partial-leading-underscore': true,
+    'scss/at-mixin-pattern': '^[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
+    'scss/at-rule-no-unknown': true,
+    'scss/dollar-variable-colon-newline-after': 'always',
+    'scss/dollar-variable-colon-space-after': 'always',
+    'scss/operator-no-newline-before': true,
+    'scss/operator-no-unspaced': true,
+    'scss/selector-no-redundant-nesting-selector': true,
     'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': true,
     'csstree/validator': true,
